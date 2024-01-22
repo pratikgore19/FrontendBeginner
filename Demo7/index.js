819f51a9801fc7e16797413bc373c3e9d0b2c7db
pratikgore19 <pratikgore986@gmail.com>
2024-01-22 14:43:01 +0530

    Alphabet Project

--- Demo7/index.js
+++ Demo7/index.js
@@ -6,7 +6,28 @@ function showSlideBar() {
         sliderBar.classList.remove('show');
     }
     else {
-        sliderBar.classList.add('show');    }
+        sliderBar.classList.add('show');
+    }
+}
+hamburgerBtn.addEventListener('click', showSlideBar);
+
+function playAudio(event) {
+    let audio = new Audio(`/Demo7/Asset/${event.key.toUpperCase()}.wav`);
+    audio.play();
 }
 
-hamburgerBtn.addEventListener('click', showSlideBar);
+document.body.addEventListener("keydown", function(e) {
+    playAudio(e);
+    document.getElementById("key").innerHTML = e.key.toUpperCase();
+
+    document.getElementById("key").style.backgroundColor = "#00dfc4";
+    document.getElementById("key").style.color = "#223243";
+    document.getElementById("key").style.boxShadow = "8px 8px 8px black";
+})
+
+document.body.addEventListener("keyup", function(e) {
+    document.getElementById("key").innerHTML = "";
+    document.getElementById("key").style.backgroundColor = "#e9edf3"
+    document.getElementById("key").style.color = "#00dfc4";
+    document.getElementById("key").style.boxShadow = "none";
+})
